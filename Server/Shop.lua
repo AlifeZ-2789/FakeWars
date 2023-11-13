70f65e3cce276689227faffd365308b0e8492f03
AlifeZ-2789 <150269348+AlifeZ-2789@users.noreply.github.com>
2023-11-13 10:28:24 -0500

    Update Shop.lua

--- Server/Shop.lua
+++ Server/Shop.lua
@@ -6,9 +6,16 @@ local ShopManager = require(ReplicatedStorage.Modules.ShopManager)
 -- adding items
 local items = {}
 
-for i,v in pairs(
+for i,v in pairs(items) do
+	ShopManager:addItem(table.unpack(v))
+end
 
 ShopEvent.OnServerEvent:Connect(function(plr, item_name)
-	
+	local success, e = ShopManager:buyItem(plr, item_name)
+	if not success and e then
+		warn(e)
+	else
+		print("Success!")
+	end
 end
 	
