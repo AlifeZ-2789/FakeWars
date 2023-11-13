92c85641243cefe8d18bdde2aa798a27151d99f9
AlifeZ-2789 <150269348+AlifeZ-2789@users.noreply.github.com>
2023-11-13 14:01:57 -0500

    Update ShopView.lua

--- Client/ShopView.lua
+++ Client/ShopView.lua
@@ -2,6 +2,13 @@ local Frame = script.Parent
 local Buy = Frame.Buy
 local Data = Frame.Data
 
+
+local ItemView = Frame.ItemView
+local ItemNameLabel = Frame.ItemName
+local ItemQuantity = Frame.ItemQuantity
+local Cost = Frame.Cost
+
+
 local data = ShopManager:getItem("wool_white")
 
 Data.OnInvoke = function(_data)
@@ -15,9 +22,14 @@ Buy.OnInvoke = function()
 	ShopEvent:FireServer(data.item_name)
 end
 
+local function clearView() end
+
+local lastData = data
+
 local currentThread = coroutine.create(function()
 	while true do
 		task.wait()
+		if lastData == nil or lastData == data then continue end
 		
 	end
 end)
