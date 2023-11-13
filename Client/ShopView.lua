28e0e27a2521fec61967755dd5bf3963d2cb6bb8
AlifeZ-2789 <150269348+AlifeZ-2789@users.noreply.github.com>
2023-11-13 10:37:42 -0500

    Create ShopView.lua

---
+++ Client/ShopView.lua
@@ -0,0 +1,16 @@
+local Frame = script.Parent
+local Buy = Frame.Buy
+local Data = Frame.Data
+
+local data = ShopManager:getItem("wool_white")
+
+Data.OnInvoke = function(_data)
+	if _data then
+		data = _data
+	end
+	return data
+end
+
+Buy.OnInvoke = function()
+	ShopEvent:FireServer(data.item_name)
+end
