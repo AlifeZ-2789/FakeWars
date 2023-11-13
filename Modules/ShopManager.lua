9c66130839340782879ad538c3e76173d08d34c3
AlifeZ-2789 <150269348+AlifeZ-2789@users.noreply.github.com>
2023-11-13 10:27:02 -0500

    Update ShopManager.lua
    Added:
    :hasItem
    :getItem
    
    Changes to buy item

--- Modules/ShopManager.lua
+++ Modules/ShopManager.lua
@@ -5,18 +5,22 @@ _G.shopItems = _G.shopItems or {}
 
 function ShopManager:buyItem(plr, shop_item)
 	if typeof(shop_item) ~= "table" then
-		shop_item = _G.shopItems[shop_item]
+		shop_item = ShopManager:getItem(shop_item)
+		if not shop_item then
+			return false, "Item doesn't exist"
+		end
 	end
+	if not ShopManager:hasItem(shop.item_name or "nil") then return false, "Item doesn't exist" end
 	local cost_item = shop_item.cost_item
 	local cost = shop_item.cost
-	if not plr:GetAttribute(cost_item) or plr:GetAttribute(cost_item) >= cost then return false end
+	if not plr:GetAttribute(cost_item) or plr:GetAttribute(cost_item) >= cost then return false, "Insufficient funds" end
 	plr:SetAttribute(cost_item, plr:GetAttribute(cost_item) - cost)
 	shop_item.buy(plr)
-	return true
+	return true, ""
 end
 
 function ShopManager:addItem(item_name, item_quantity, cost_item, cost, bought_function)
-	if ShopManager:getItem(item_name) then return false end
+	if ShopManager:getItem(item_name) then return false, "Item already exists" end
 	bought_function = bought_function or function(plr)
 		plr:SetAttribute(item_name, (plr:GetAttribute(item_name) or 0) + quantity)
 	end
